b9534ef36bf4befc4135ec626e971a0fde0b503c
flyq <flyq951@gmail.com>
2023-10-26 21:31:09 +0800

    Update main.rs

--- src/bellpepper/linear_combination/src/main.rs
+++ src/bellpepper/linear_combination/src/main.rs
@@ -3,27 +3,26 @@ use blstrs::Scalar;
 use ff::Field;
 
 fn main() {
-    println!("Hello, world!");
     lc_add_simplify();
 }
 
 fn lc_add_simplify() {
-    let size = 5;
-    let mut lc = LinearCombination::<Scalar>::zero();
+    let mut lc1 = LinearCombination::<Scalar>::default();
+    let mut lc2 = LinearCombination::<Scalar>::default();
+    let mut lc3 = LinearCombination::<Scalar>::default();
 
-    let mut expecteds = vec![Scalar::ZERO; size];
-    let mut total_additions = 0;
-    for (i, expected) in expecteds.iter_mut().enumerate() {
-        for _ in 0..i + 1 {
-            let coeff = Scalar::ONE;
-            lc = lc + (coeff, Variable::new_unchecked(Index::Aux(i)));
-            *expected += coeff;
-            total_additions += 1;
-        }
-    }
+    let one = Scalar::ONE;
+    let two = one + one;
 
-    assert_eq!(size, lc.len());
-    assert_ne!(lc.len(), total_additions);
+    lc1 = lc1 + (one, Variable::new_unchecked(Index::Aux(0)));
 
-    println!("{:?}", lc);
+    lc2 = lc2 + (two, Variable::new_unchecked(Index::Aux(1)));
+    lc2 = lc2 + (one, Variable::new_unchecked(Index::Aux(2)));
+    lc2 = lc2 + (one, Variable::new_unchecked(Index::Aux(1)));
+
+    lc3 = lc3 + (two, Variable::new_unchecked(Index::Input(3)));
+
+    lc3 = lc3 + &lc2;
+
+    println!("{:?}\n{:?}\n{:?}", lc1, lc2, lc3);
 }
